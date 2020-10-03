a2f0c6b4ca7b0d47718c5e9c78adce04358931d3
Jan Poonthong <janpoonthong628@gmail.com>
2020-10-03 16:47:44 +0700

    Add update to vim and comic-shanns

--- scripts/update_apt.sh
+++ scripts/update_apt.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+# Apt
+echo 'Apt'
 sudo apt update && sudo apt upgrade -y
 sudo apt list --upgradable
 sudo apt dist-upgrade -y
 sudo apt clean
 sudo apt autoremove
+
+# Vim
+echo 'Vim'
+cd ~/Downloads/vim/ && git fetch && git pull
+echo 'Font'
+cd ~/Downloads/font/comic-shanns/ && git fetch && git pull

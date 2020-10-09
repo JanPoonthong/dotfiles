ee975bf027b0382733721d8ac9a878b9185c81b5
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2020-10-09 08:15:38 +0700

    Script: Add path to fzf and gfp

--- scripts/update_apt.sh
+++ scripts/update_apt.sh
@@ -11,5 +11,9 @@ sudo apt autoremove
 # Vim
 echo 'Vim'
 cd ~/Downloads/vim/ && git fetch && git pull
+# Font
 echo 'Font'
 cd ~/Downloads/fonts/comic-shanns/ && git fetch && git pull
+# FZF
+echo 'FZF'
+cd ~/.fzf/ && git fetch && git pull

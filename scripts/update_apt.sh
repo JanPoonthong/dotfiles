9e58b5f99eef086b2df037c60c449fc185db0fc0
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2020-10-16 13:47:35 +0700

    Add echo 'Pip' to update_pip.sh

--- scripts/update_apt.sh
+++ scripts/update_apt.sh
@@ -8,12 +8,9 @@ sudo apt dist-upgrade -y
 sudo apt clean
 sudo apt autoremove
 
-# Vim
 echo 'Vim'
 cd ~/Downloads/vim/ && git fetch && git pull
-# Font
 echo 'Font'
 cd ~/Downloads/fonts/comic-shanns/ && git fetch && git pull
-# FZF
 echo 'FZF'
 cd ~/.fzf/ && git fetch && git pull
--- scripts/update_pip.sh
+++ scripts/update_pip.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+echo 'Pip'
 pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U

d860c9db7a15a95d37b59afd6a53a71e9f5d1002
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2020-11-03 19:19:39 +0700

    Delete update_git.sh

--- scripts/update_apt.sh
+++ scripts/update_apt.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-# Apt
 echo 'Apt'
 sudo apt update && sudo apt upgrade -y
 sudo apt list --upgradable
-sudo apt dist-upgrade -y
-sudo apt clean
 sudo apt autoremove
--- scripts/update_git.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo 'Vim'
-cd ~/Downloads/vim/ && gfp
-echo 'FZF'
-cd ~/.fzf/ && gfp

7d3a50687c03c222869fa53d6348b6e5b0af7d70
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2021-01-07 14:07:34 +0700

    Delete add-apt-repository

--- script/apt_needs.sh
+++ script/apt_needs.sh
@@ -15,6 +15,3 @@ sudo apt install sqlite3
 sudo apt install python3
 sudo apt install mpg123
 sudo apt install ffmpeg
-sudo add-apt-repository ppa:obsproject/obs-studio
-sudo apt update
-sudo apt install obs-studio

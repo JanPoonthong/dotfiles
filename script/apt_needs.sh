0aa434353617a2fd2f69f1bc27b1a478a512e16e
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2021-01-09 08:30:46 +0700

    Add into one liner

--- script/apt_needs.sh
+++ script/apt_needs.sh
@@ -1,17 +1,1 @@
-sudo apt install flake8
-sudo apt install htop
-sudo apt install zsh
-sudo apt install tree
-sudo apt install xclip
-sudo apt install ncdu
-sudo apt install git
-sudo apt install python3-pygame
-sudo apt install python3-tk
-sudo apt install python3-venv python3-pip
-sudo apt install vim
-sudo apt install libpq-dev
-sudo apt install vim-gtk
-sudo apt install sqlite3
-sudo apt install python3
-sudo apt install mpg123
-sudo apt install ffmpeg
+sudo apt install flake8 htop zsh tree xclip ncdu git python3-pygame python3-tk python3-venv python3-pip vim libpq-dev vim-gtk sqlite3 mpg123 ffmpeg

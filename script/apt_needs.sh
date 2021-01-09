ea3647e13805e8d0c2b4808f6fbdfaacdba7d7ae
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2021-01-09 09:08:20 +0700

    Add more apt

--- script/apt_needs.sh
+++ script/apt_needs.sh
@@ -1,1 +1,1 @@
-sudo apt install flake8 htop zsh tree xclip ncdu git python3-pygame python3-tk python3-venv python3-pip vim libpq-dev vim-gtk sqlite3 mpg123 ffmpeg
+sudo apt install pngcrush flake8 htop zsh tree xclip ncdu git python3-pygame python3-tk python3-venv python3-pip vim libpq-dev vim-gtk sqlite3 mpg123 ffmpeg cmake ninja-build build-essential libsdl2-dev

4d9cb3993cfd5c10b4ad03d3f198418642780905
Jan Poonthong <janpoonthong628@gmail.com>
2021-01-20 09:59:51 -0500

    Remove all

--- README.md
+++
@@ -1,2 +0,0 @@
-# Dotfiles
-This is all dotfiles (vim, bash and zsh). If you want to use my vim, feel free, I use vim to write all my codes
--- gnome-terminal/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e -E
-
-HERE="$(cd "$(dirname "${0}")" && pwd)"
-
-/usr/bin/dconf dump /org/gnome/terminal/ >"${HERE}/gnome-terminal.ini"
-dconf load /org/gnome/terminal/ < gnome-terminal.ini

4bfefc35238f2bdc730c1563c264d599b5597641
Jan Poonthong <janpoonthong628@gmail.com>
2021-04-20 09:23:12 +0700

    [apt] Add tmux

--- script/apt_needs.sh
+++ script/apt_needs.sh
@@ -1,1 +1,1 @@
-sudo apt install ripgrep gimp flake8 htop zsh tree xclip ncdu git python3-venv python3-pip sqlite3
+sudo apt install ripgrep gimp flake8 htop zsh tree xclip ncdu git python3-venv python3-pip sqlite3 tmux

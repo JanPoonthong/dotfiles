723ab1af4f377e232bee8c4c4c83e922e4762870
Jan Poonthong <janpoonthong628@gmail.com>
2021-04-20 21:37:50 +0700

    Remove things that I don't understand

--- script/apt_needs.sh
+++ script/apt_needs.sh
@@ -1,1 +1,1 @@
-sudo apt install ripgrep gimp flake8 htop zsh tree xclip ncdu git python3-venv python3-pip sqlite3 tmux
+sudo apt install ripgrep flake8 htop zsh tree xclip ncdu git python3-venv python3-pip sqlite3

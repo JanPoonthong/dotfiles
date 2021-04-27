9fd6c4dd722bd142db82eda8bb11fab6556897d2
Jan Poonthong <janpoonthong628@gmail.com>
2021-04-27 09:49:30 +0700

    [script] One install.sh does all the jobs

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-ln -sf $PWD/vim ~/.vim && ln -sf $PWD/zsh ~/.zsh && ln -sf $PWD/zsh/zshrc ~/.zshrc && ln -sf $PWD/vim/vimrc ~/.vimrc
+ln -sf $PWD/vim ~/.vim && ln -sf $PWD/zsh ~/.zsh && ln -sf $PWD/zsh/zshrc ~/.zshrc && ln -sf $PWD/vim/vimrc ~/.vimrc && cd git/ && ./install.sh

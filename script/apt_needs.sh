0e2b66e31eb3ae86aae0b33d6b0ec70fc1455683
Jan Poonthong <janpoonthong628@gmail.com>
2021-10-17 12:22:49 +0700

    Add gparted

--- script/apt_needs.sh
+++ script/apt_needs.sh
@@ -1,1 +1,1 @@
-sudo apt install ripgrep flake8 htop zsh tree xclip ncdu git python3-venv python3-pip sqlite3
+sudo apt install ripgrep flake8 htop zsh tree xclip ncdu git python3-venv python3-pip sqlite3 gparted
